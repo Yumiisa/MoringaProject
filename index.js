0a9ff049b0dc7028812d1fde3bc3204b4482295f
caudencia wanyama <conny@students.uonbi.ac.ke>
2022-06-25 00:15:43 +0300

    Grabbed all element required for dom manipulation in index.js

---
+++ index.js
@@ -0,0 +1,5 @@
+//grab all element required for dom manipulation
+const searchBtn = document.getElementById('search-btn');
+const mealList = document.getElementById('meal');
+const mealDetailsContent = document.querySelector('.meal-details-content');
+const recipeCloseBtn = document.getElementById('recipe-close-btn');
