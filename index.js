aff5d22779c6dc626f59586836f60198815fa3ab
caudencia wanyama <conny@students.uonbi.ac.ke>
2022-06-25 00:24:10 +0300

    fectching data

--- index.js
+++ index.js
@@ -7,3 +7,5 @@ const recipeCloseButton = document.getElementById('recipe-close-btn');
 searchButton.addEventListener('click', getMealLists);
 function getMealLists(){
 let searchInputTxt = document.getElementById('search-input').value.trim();
+function getMealList(){
+    let searchInputTxt = document.getElementById('search-input').value.trim();
