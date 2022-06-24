ec6dbd1bba63ccd6c40b29c7649f2b0f37fe8b66
caudencia wanyama <conny@students.uonbi.ac.ke>
2022-06-25 00:22:02 +0300

    grabed search input class to be used in fetching data from the server

--- index.js
+++ index.js
@@ -3,4 +3,7 @@ const searchButton = document.getElementById('search-btn');
 const mealLists = document.getElementById('meal');
 const mealDetails = document.querySelector('.meal-details-content');
 const recipeCloseButton = document.getElementById('recipe-close-btn');
+//event listener
 searchButton.addEventListener('click', getMealLists);
+function getMealLists(){
+let searchInputTxt = document.getElementById('search-input').value.trim();
