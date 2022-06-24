311d91b1fad531db1d2ad1b148c9badc0f08028f
caudencia wanyama <conny@students.uonbi.ac.ke>
2022-06-25 00:28:18 +0300

    looping through data from server to get each meal append in html file

--- index.js
+++ index.js
@@ -7,5 +7,22 @@ const recipeCloseButton = document.getElementById('recipe-close-btn');
 searchButton.addEventListener('click', getMealLists);
 function getMealLists(){
 let searchInputTxt = document.getElementById('search-input').value.trim();
-function getMealList(){
-    let searchInputTxt = document.getElementById('search-input').value.trim();
+fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${searchInputTxt}`)
+    .then(response => response.json())
+    .then(data => {
+        //declare variable to store my data
+        let myData = "";
+        if(data.meals){
+            data.meals.forEach(meal => {
+                myData += `
+                    <div class = "meal-item" data-id = "${meal.idMeal}">
+                        <div class = "meal-img">
+                            <img src = "${meal.strMealThumb}" alt = "food">
+                        </div>
+                        <div class = "meal-name">
+                            <h3>${meal.strMeal}</h3>
+                            <a href = "#" class = "recipe-btn">Get Recipe</a>
+                        </div>
+                    </div>
+                `;
+            });
