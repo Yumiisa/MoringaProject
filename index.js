d6b5282f63395aa2eff00151545f6bfa331af606
caudencia wanyama <conny@students.uonbi.ac.ke>
2022-06-25 15:04:08 +0300

    added close button on my menu

--- index.js
+++ index.js
@@ -54,7 +54,7 @@ fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${searchInputTxt}`)
 function mealRecipe(meal){
     console.log(meal);
     meal=meal[0];
-    let myData = `
+    let myMeal = `
     <h2 class = "recipe-title">${meal.strMeal}</h2>
     <p class = "recipe-category">${meal.strCategory}</p>
     <div class = "recipe-instruct">
@@ -68,10 +68,46 @@ function mealRecipe(meal){
         <a href = "${meal.strYoutube}" target = "_blank">Watch Video</a>
     </div>
 `;
-mealDetails.innerHTML = myData;
+mealDetails.innerHTML =myMeal;
 mealDetails.parentElement.classList.add('showRecipe');
 }
 
 
 
-    
+    /*
+    function getMealRecipe(e){
+    e.preventDefault();
+    if(e.target.classList.contains('recipe-btn')){
+        let mealItem = e.target.parentElement.parentElement;
+        fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealItem.dataset.id}`)
+        .then(response => response.json())
+        .then(data => mealRecipeModal(data.meals));
+    }
+}
+
+// create a modal
+function mealRecipeModal(meal){
+    console.log(meal);
+    meal = meal[0];
+    let html = `
+        <h2 class = "recipe-title">${meal.strMeal}</h2>
+        <p class = "recipe-category">${meal.strCategory}</p>
+        <div class = "recipe-instruct">
+            <h3>Instructions:</h3>
+            <p>${meal.strInstructions}</p>
+        </div>
+        <div class = "recipe-meal-img">
+            <img src = "${meal.strMealThumb}" alt = "">
+        </div>
+        <div class = "recipe-link">
+            <a href = "${meal.strYoutube}" target = "_blank">Watch Video</a>
+        </div>
+    `;
+    mealDetailsContent.innerHTML = html;
+    mealDetailsContent.parentElement.classList.add('showRecipe');
+}
+mealList.addEventListener('click', getMealRecipe);
+recipeCloseBtn.addEventListener('click', () => {
+    mealDetailsContent.parentElement.classList.remove('showRecipe');
+});
+*/
